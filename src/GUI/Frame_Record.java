323dd81e49e6c9b45a0e84ae60069f006d556589
jkwange <diahdkdhd1@gmail.com>
2023-12-24 03:54:25 +0900

    대전 전적기록 Ranking GUI 세팅 함

--- src/GUI/Frame_Record.java
+++ src/GUI/Frame_Record.java
@@ -1,78 +1,85 @@
 package GUI;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-
 import java.awt.*;
-import java.awt.event.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import student.*;
 
-import student.Record_Manager;
-import student.SInfo;
-import student.StudentInfoManager;
+public class Frame_Record extends JFrame {
+    JButton update;
+    private StudentInfoManager SIM;
+    private Record_Manager RM;
+    private DefaultTableModel model;
+    private JTable recordTable;
+    private JScrollPane recordScrlp;
 
+    public Frame_Record(StudentInfoManager ref) {
+        SIM = ref;
+        RM = new Record_Manager(SIM);
+        setTitle("전적 기록");
+        setSize(300, 200);
+        Vector<String> column = new Vector<String>();
+        column.add("흑 이름"); column.add("백 이름"); column.add("대국 결과");
+        Vector<Vector<String>> recordData = RM.getRecord();
+        model = new DefaultTableModel(recordData, column) {
+            @Override
+            public boolean isCellEditable(int row, int mCol) {
+                return false;
+            }
+        };
+        recordTable = new JTable(model);
 
+        // recordTable에 렌더러 추가하기
+        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                Component c= super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                
+                c.setBackground(Color.decode("#EEEEEE"));
+                String gameResult = (String) table.getValueAt(row, 2);
 
-public class Frame_Record extends JFrame{
-	private JTable rcdTable; // 랭킹 출력할 테이블
-	private JScrollPane scrlP; // 랭킹 테이블을 얹을 스크롤팬
-	private StudentInfoManager SIM;
-	private Record_Manager RM = new Record_Manager(SIM);
-	private DefaultTableModel model;
-	
-	
-	public Frame_Record(StudentInfoManager ref) {
-		SIM = ref;
-		setTitle("Ranking");
-		setSize(300, 200);
-		String[] columnName = {"등수", "이름", "학과", "승률" };
-		String[][] rankingData = SIM.getRanking();
-		/*
-		model = new DefaultTableModel(rankingData, columnName) {
-			@Override
-			public boolean isCellEditable(int row, int mCol) {
-				return false;
-			}
-		};
-		
-		rnkTable = new JTable(model);
-	
-		scrlP = new JScrollPane(rnkTable);
-		add(scrlP);
-		*/
-		setVisible(true);
-	}
-	
-	
+                
+                if (column == 0) {
+                    c.setBackground(Color.BLACK); // 특정 열의 배경색 설정
+                    c.setForeground(Color.WHITE); // 특정 열의 폰트 색상 설정
+                }
+                else if (column == 1) {
+                	 c.setBackground(Color.WHITE); // 특정 열의 배경색 설정
+                     c.setForeground(Color.BLACK);
+                }
+                // 조건에 따라 배경색 변경
+                else if (gameResult.equals("백 승")) {
+                   // c.setBackground(Color.decode("#FFFFCC"));
+                	c.setBackground(Color.decode("#FFFFFF"));
+                    gameResult = "흑 승";
+                } else if (gameResult.equals("흑 승")) {
+                    //c.setBackground(Color.decode("#FFCC99"));
+                	c.setBackground(Color.decode("#000000"));
+                	c.setForeground(Color.WHITE);
+                    gameResult = "백 승";
+                } else if (gameResult.equals("무승부")) {
+                    //c.setBackground(Color.decode("#FFFF99"));
+                	c.setBackground(Color.decode("#888888"));
+                    gameResult = "무승부";
+                } else {
+                    c.setBackground(table.getBackground());
+                }
+
+                return c;
+            }
+        };
+
+        
+        
+        // 특정 열의 셀 렌더러 설정 (대국 결과 열에 해당하는 인덱스는 2)
+        recordTable.getColumnModel().getColumn(2).setCellRenderer(renderer);
+        recordTable.getColumnModel().getColumn(1).setCellRenderer(renderer);
+        recordTable.getColumnModel().getColumn(0).setCellRenderer(renderer);
+        recordTable.setBackground(Color.decode("#000000"));
+        recordScrlp = new JScrollPane(recordTable);
+        getContentPane().add(recordScrlp);
+        setVisible(true);
+    }
 }
-
-/*
- * 	private JTable rnkTable; // 랭킹 출력할 테이블
-	private JScrollPane scrlP; // 랭킹 테이블을 얹을 스크롤팬
-	private StudentInfoManager SIM;
-	private DefaultTableModel model;
-	public Frame_Ranking(StudentInfoManager ref){
-		SIM = ref;
-		setTitle("Ranking");
-		setSize(300, 200);
-		String[] columnName = {"등수", "이름", "학과", "점수" };
-		String[][] rankingData = SIM.getRanking();
-		
-		model = new DefaultTableModel(rankingData, columnName) {
-			@Override
-			public boolean isCellEditable(int row, int mCol) {
-				return false;
-			}
-		};
-		
-		rnkTable = new JTable(model);
-	
-		scrlP = new JScrollPane(rnkTable);
-		add(scrlP);
-		setVisible(true);
-	}
-	
- * */
--- src/student/Record_Manager.java
+++ src/student/Record_Manager.java
@@ -1,79 +1,84 @@
 package student;
 
+
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.*;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Record_Manager extends JFrame{
 	private StudentInfoManager SIM;
 	private Vector<Vector<String>> Records = new Vector<Vector<String>>();
 	public Record_Manager(StudentInfoManager ref) {
+		SIM = ref;
 		readRecord();
 	}
-	
-	private static final String FILE_NAME = "대국결과.txt";
+	private static final String FILE_Record = "대국결과.txt";
+	// 현재 사용자의 홈 디렉토리를 얻어옴
 	String userHome = System.getProperty("user.home");
+	// 바탕화면 디렉토리 경로 조합
 	Path desktopPath = Paths.get(userHome, "Desktop");
-	String filePath = desktopPath.resolve(FILE_NAME).toString();
+	String fileRecord = desktopPath.resolve(FILE_Record).toString();
 	
 	public void readRecord() { // 대국결과 기록 파일을 배열에 저장
-	    try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8))) {
-	        String line;
-	        while ((line = br.readLine()) != null) {
-	            parseRecord(line);
-	        }
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-    	
-    	Iterator<Vector<String>> it = Records.iterator();
-    	while(it.hasNext()) {
-    		Iterator<String> it2 = it.next().iterator();
-    		while(it2.hasNext()) {
-    			System.out.print(it2.next());
+    	try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileRecord), StandardCharsets.UTF_8))) {
+    		String line;	
+    		while((line = br.readLine()) != null) {
+    			parseRecord(line);
     		}
-    		System.out.println();
+    	} catch(IOException e) {
+    		e.printStackTrace();
     	}
 	}
 	    
 	public void parseRecord(String line) { // line에서 승패정보 가져와서 rating update
-		Vector<String> lineV = new Vector<String>();
     	String[] parts = line.split(",");
+    	String bName;
+    	String wName;
+    	String result;
     	try {
     		if(parts.length == 3) {
-	    		lineV.add(parts[0].trim());
-	    		lineV.add(parts[1].trim());
-	    		lineV.add(parts[2].trim());
+	    		bName = parts[0].trim();
+	    		wName = parts[1].trim();
+	    		if(parts[2].trim().equals("1")) result = "흑 승";
+	    		else if(parts[2].trim().equals("0")) result = "백 승";
+	    		else result = "무승부";
+	    		addRecord(bName, wName, result);
     		} else 
 	    		System.out.println("잘못된 형식의 라인 : " + line);
 	    		
     	} catch (NumberFormatException e) {
     		return;
     	}
-    	Records.add(lineV);
     }
 	    
-	    /*
-	    public String[][] getRanking() {
-	    	return ranking;
-	    }
-	    
-	    public String[] searchMember(int id) {
-	    	// 학번 정보 받아서 해당하는 랭킹 행 출력
-	    	SInfo info = Members.get((Integer)id);
-	    	int i;
-	    	for(i = 0; i < ranking.length; i++) {
-	    		System.out.println(info.getName().equals(ranking[i][1]));
-	    		if(info.getName().equals(ranking[i][1]))
-	    			return ranking[i];
-	    	}
-	    	return null;
-	    }
-	    */
-	//전적 저장
-	//파일에서 가저오는 함수
-	//사용자에게 입력 받아서 저장하는 함수
-	//저장된 데이터를 파일에 쓰는 함수
+	public void addRecord(String bName, String wName, String matchresult) {
+		Vector<String> lineV = new Vector<String>();
+		lineV.add(bName);
+		lineV.add(wName);
+		lineV.add(matchresult);
+		Records.add(lineV);
+	}
+	
+	public Vector<Vector<String>> getRecord(){
+		return Records;
+	}
+	
+	public void writeRecord() {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileRecord))) {
+			// 전적 기록을 파일에 저장
+				String line;
+				Iterator<Vector<String>> it1 = Records.iterator();
+				while(it1.hasNext()) {
+					Iterator<String> it2 = it1.next().iterator();
+					line = it2.next()+","+it2.next()+","+it2.next();
+				}
+		} catch (IOException e) {
+			e.printStackTrace();
+        }
+    }
+	
 }
